e03c8f2c0c5e8efa1f6c7508dedb29117052ba1e
csilvagetronics <151559865+csilvacas@users.noreply.github.com>
2023-12-16 20:55:33 +0100

    Revision calidad persistencia
    Revision calidad de código en persistencia.py después de pasar Pylint.

--- persistencia.py
+++ persistencia.py
@@ -1,5 +1,6 @@
+""" Función guardar pedido """
 def guardar_pedido(nombre, apellidos):
-
+    """ Escribir datos en fichero pedidos.txt"""
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
