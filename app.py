13051b148a0c27dbc7ab7c0f26b199bd9feecced
csilvagetronics <151559865+csilvacas@users.noreply.github.com>
2023-12-18 19:29:11 +0100

    ajuste unificación

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def checksize():
     Comprueba disponibilidad de un tamaño de pizza.
     """
     size = request.form.get("tamano")
-    if size == 'S':
+    if size == "S":
         mensaje = 'No disponible'
     else:
         mensaje = 'Disponible'
